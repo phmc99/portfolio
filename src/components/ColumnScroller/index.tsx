5afbd34b0ef91985a8f9e258dea17851e396660d
Pedro Costa <phmc99@outlook.com>
2025-02-03 22:34:52 -0300

    WIP - Styling projects columns

--- src/components/ColumnScroller/index.tsx
+++ src/components/ColumnScroller/index.tsx
@@ -9,7 +9,7 @@ export default function ColumnScroller({ columns }: IColumnScroller) {
   return (
     <div className="relative">
       <div className="absolute left-0 top-0 h-full w-4 bg-gradient-to-r from-zinc-900 via-[#18181b9a] to-transparent backdrop-blur-xs lg:hidden"></div>
-      <div className="flex gap-4 w-full overflow-x-scroll md:overflow-auto px-6 lg:px-0">
+      <div className="flex gap-4 w-full overflow-x-scroll md:overflow-hidden px-6 lg:px-0">
         {columns.map((Column, index) => (
           <ColumnScrollerBase key={`column-scoller-${index}`}>
             {<Column />}
--- src/components/Craft/index.tsx
+++ src/components/Craft/index.tsx
@@ -1,9 +1,15 @@
+import { ubuntu } from "@/app/fonts";
+
 export default function Craft() {
   return (
-    <div className="w-[80vw] max-w-64 lg:w-[99%] border-solid border-2 border-red-500 ">
-      <h6>Construindo</h6>
-      <h2>Craft</h2>
-      <p>Pequenas implementações de interfaces e interações</p>
+    <div className="w-[80vw] max-w-64 lg:w-[99%] border-solid">
+      <h6 className="text-sm text-zinc-500">Construindo</h6>
+      <div className="py-3"></div>
+      <h2 className="text-lg">Craft</h2>
+      <div className="py-0.5"></div>
+      <p className={`${ubuntu.className} text-zinc-400`}>
+        Pequenas implementações de interfaces e interações
+      </p>
     </div>
   );
 }
