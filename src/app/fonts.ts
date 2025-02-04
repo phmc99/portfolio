121ae902486882159a639c623ca6d2bae903db3c
Pedro Costa <phmc99@outlook.com>
2025-02-04 15:51:30 -0300

    WIP - Now/About me section

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,12 +4,19 @@ export const marcellus = Marcellus({
   weight: ["400"],
   display: "auto",
   preload: true,
+  subsets: ["latin"],
 });
 
 export const ubuntu = Ubuntu({
   display: "auto",
   weight: ["300", "400", "500", "700"],
   preload: true,
+  subsets: ["latin"],
 });
 
-export const prata = Prata({ weight: ["400"], display: "auto", preload: true });
+export const prata = Prata({
+  weight: ["400"],
+  display: "auto",
+  subsets: ["latin"],
+  preload: true,
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Craft from "@/components/Craft";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col  justify-center gap-4 lg:w-1/2 max-w-[920px] lg:mx-auto px-6 pt-20 pb-10 md:px-16 lg:px-0">
+      <section className="flex flex-col  justify-center gap-2 lg:w-1/2 max-w-[920px] lg:mx-auto px-6 pt-20 pb-10 md:px-16 lg:px-0">
         <h1 className={`${marcellus.className} text-2xl md:text-3xl`}>
           Pedro Costa
         </h1>
-        <p className={`${prata.className} text-sm md:text-base`}>
+        <p className={`${prata.className} text-sm md:text-base text-zinc-500`}>
           Resolvendo problemas e construindo o futuro com a tecnologia.
         </p>
-        <p className={`${ubuntu.className} text-sm/6 md:text-base/6`}>
+        <p className={`${ubuntu.className} text-sm/6 md:text-base/6 mt-4`}>
           <span className="font-bold">
             Desenvolvedor Full Stack com 4 anos de experiência.
           </span>{" "}
@@ -21,9 +21,38 @@ export default function Home() {
           a publicação, além de adicionar novas funcionalidades, realizar
           manutenção e oferecer suporte ao cliente para esses tipos de sistema.
         </p>
-      </div>
+      </section>
+
       <section className="px-0 lg:w-1/2 max-w-[920px] lg:mx-auto">
         <ColumnScroller columns={[Craft, Craft, Craft]} />
+      </section>
+
+      <section className="flex flex-col  justify-center gap-4 lg:w-1/2 max-w-[920px] lg:mx-auto px-6 pt-20 pb-10 md:px-16 lg:px-0">
+        <h3 className={`${marcellus.className} text-lg md:text-xl`}>Agora</h3>
+        <p className={`${ubuntu.className} text-sm/6 md:text-base/6`}>
+          Procurando desenvolver minhas habilidades resolvendo problemas,
+          explorando tecnologias e interesses, entregando isso com qualidade.
+          Sabendo que{" "}
+          <span className={`${prata.className} underline underline-offset-4`}>
+            tudo ao meu redor é o trabalho da vida de alguém
+          </span>
+          .
+        </p>
+        <p className={`${ubuntu.className} text-sm md:text-base`}>
+          Tudo que eu quero é construir e melhorar sistemas. APIs, Front-ends,
+          Webhooks, Apps, etc. A programação tem infinitas oportunidades de
+          criar e sinto que ainda tenho muita coisa para experimentar.
+        </p>
+        <p className={`${ubuntu.className} text-sm/6 md:text-base/6`}>
+          Um pouco sobre mim. Sou praticante de Jiu-jitsu e Judô, já fui
+          competidor de ambos por algum tempo, também tive uma fase como
+          professor e gosto bastante da filosofia por trás dessas artes. Também
+          gosto de musculação, considero um complemento aos meus treinos de luta
+          e me sinto bem ao fazer.{" "}
+          <span className={`${prata.className} text-zinc-700`}>
+            Pratiquem esporte, bebam água e comam bem hahahaha
+          </span>
+        </p>
       </section>
     </main>
   );
