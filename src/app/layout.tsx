a1c1e93cb678342c7605435c26859267f6109d88
Pedro Costa <phmc99@outlook.com>
2025-02-11 23:48:09 -0300

    STYLE - Top fade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,13 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <Analytics />
-      <body className="relative bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
+      <body className="bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
+        <div className="z-10 fixed top-0 h-20 w-full bg-gradient-to-b from-zinc-900 via-zinc-900/80 to-transparent backdrop-blur-xs"></div>
         {children}
         <div className="pb-12"></div>
-        <Footer />
+        <div className="relative">
+          <Footer />
+        </div>
       </body>
     </html>
   );
