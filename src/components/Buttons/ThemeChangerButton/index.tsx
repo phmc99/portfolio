20c3b463eab01deb889009653521c12c705f414d
Pedro Costa <phmc99@outlook.com>
2025-02-12 18:57:44 -0300

    WIP - Menu of important buttons

--- src/components/Buttons/ThemeChangerButton/index.tsx
+++ src/components/Buttons/ThemeChangerButton/index.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 const ThemeChangerButton = () => {
   const { theme, setTheme } = useTheme();
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="flex flex-col">
-      The current theme is: {theme}
-      <button onClick={() => setTheme("light")}>Light Mode</button>
-      <button onClick={() => setTheme("dark")}>Dark Mode</button>
+    <div
+      className="z-20 fixed right-0 bottom-4 w-12 h-10 bg-red-500"
+      onClick={() => setMenuOpen(!menuOpen)}
+    >
+      {menuOpen ? (
+        <div className="flex flex-col">
+          The current theme is: {theme}
+          <button onClick={() => setTheme("light")}>Light Mode</button>
+          <button onClick={() => setTheme("dark")}>Dark Mode</button>
+        </div>
+      ) : null}
     </div>
   );
 };
